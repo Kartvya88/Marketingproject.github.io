fb3cac0cc928a687ba91f449a3c9869c495f2cbd
Kartvya88 <96675668+Kartvya88@users.noreply.github.com>
2022-01-16 14:31:49 -0500

    Updated

---
+++ html.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#header").load("./inc/header.html")
+});
+    $(function(){
+        $("#footer").load("./inc/footer.html")
+    });
+    // appropriate java script for the header and footer 
